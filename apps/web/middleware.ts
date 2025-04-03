a47847d289a7d8a76849e2ae90492ffb185c4dbb
animesh <phb21013@tezu.ac.in>
2025-04-03 20:00:34 +0530

    middleware fixed

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -26,5 +26,5 @@ export async function middleware(req:NextRequest) {
 
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/dashboard'],
+  matcher: ['/dashboard/:path*', '/dashboard', '/spaces', '/spaces/:path*'],
 }
